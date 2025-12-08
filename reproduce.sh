cb2cfae3c6525e755dfef91ec7939cface1e2ad6
kunxian xia <xiakunxian130@gmail.com>
2025-12-08 15:51:51 +0800

    skip app proof generation if file exits

--- reproduce.sh
+++ reproduce.sh
@@ -4,10 +4,15 @@ export RUSTFLAGS="-C target-feature=+avx2"
 export RUST_LOG=info,ceno_zkvm=debug
 
 # 1. generate app proof in release mode
-cargo run --features "jemalloc,gpu" --config net.git-fetch-with-cli=true \
-    --bin ceno-reth-benchmark-bin --profile release -- --block-number 23587691 \
-    --rpc-url https://eth-mainnet.g.alchemy.com/v2/dpJmxzwalHmaOsZwAB-Djvz2N651QahS \
-    --cache-dir block_data --output-dir output --mode prove-app 2>&1 | tee app.log
+if [ ! -z "output/app_proof.bitcode" ]; then
+    echo "App proof already exists, skipping generation."
+else
+    # generate app proof using cpu prover for now
+    cargo run --features "jemalloc" --config net.git-fetch-with-cli=true \
+        --bin ceno-reth-benchmark-bin --profile release -- --block-number 23587691 \
+        --rpc-url https://eth-mainnet.g.alchemy.com/v2/dpJmxzwalHmaOsZwAB-Djvz2N651QahS \
+        --cache-dir block_data --output-dir output --mode prove-app 2>&1 | tee app.log
+fi
 
 # 2. compress app proofs into 1 stark proof in trial mode
 cargo run --features "jemalloc,gpu" --config net.git-fetch-with-cli=true \
