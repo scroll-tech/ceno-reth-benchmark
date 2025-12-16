5d76d416eb576c58ad90442ec92cab418c9da2bf
sm.wu <hero78119@gmail.com>
2025-12-16 11:00:41 +0800

    update dep

--- cargo_update_ceno.sh
+++ cargo_update_ceno.sh
@@ -1,1 +1,1 @@
-cargo update --config net.git-fetch-with-cli=true ceno_zkvm git+https://github.com/scroll-tech/elliptic-curves?branch=ceno%2Fk256-13.4#k256@0.13.4 git+https://github.com/scroll-tech/ceno-patch#ceno_syscall@0.1.0 cuda_hal 
+cargo update --config net.git-fetch-with-cli=true ceno_zkvm git+https://github.com/scroll-tech/elliptic-curves?branch=ceno%2Fk256-13.4#k256@0.13.4 ceno_syscall cuda_hal 
