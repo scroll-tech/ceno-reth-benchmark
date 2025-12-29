28cd340c7fac2cf0a59cc5f03bcbe29ad0030c27
sm.wu <hero78119@gmail.com>
2025-12-29 14:57:32 +0800

    update client

--- bin/ceno-client-eth/src/main.rs
+++ bin/ceno-client-eth/src/main.rs
@@ -1,7 +1,7 @@
 // use openvm::io::{println, read, reveal_bytes32};
 extern crate ceno_rt;
 use alloy_primitives::Address;
-use ceno_crypto::ceno_crypto;
+use ceno_crypto::{ceno_crypto};
 use openvm_client_executor::{io::ClientExecutorInput, ChainVariant, ClientExecutor};
 use rkyv::Archived;
 
